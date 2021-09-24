628836ccaef95cfeb38d19a428a8a7dea6e33c61
Shi521 <h.shih.159@westcliff.edu>
2021-09-24 15:50:03 -0700

    w4d2 homework

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -2,27 +2,25 @@
 
 //Function to generate combination of characters
 function generateCode() {
-    //Create variables to store generate codes and the type of characters we want to show as codes
-    //var code = ' '; //initialize to null value
-    //var str = 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqustuvwxyz0123456789@#$';
+  //Create variables to store generate codes and the type of characters we want to show as codes
+  //var code = ' '; //initialize to null value
+  //var str = 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqustuvwxyz0123456789@#$';
 
-
-//Generate character multiple times using a loop
-for ( i = 1; i <= 8; i++){
-var char = Math.random() * str.length; //random select a character from the variable and then store in a new variable
-code +=str.charAt(char)//accumlate the generated character into a string of 8 characters
-}
-return code; //return the final accumlated string when loop ends
-
+  //Generate character multiple times using a loop
+  for (i = 1; i <= 8; i++) {
+    var char = Math.random() * str.length; //random select a character from the variable and then store in a new variable
+    code += str.charAt(char); //accumlate the generated character into a string of 8 characters
+  }
+  return code; //return the final accumlated string when loop ends
 }
 
 //get HTML element to display
-document. getElementById("codes").innerHTML = generateCode();
+document.getElementById("codes").innerHTML = generateCode();
 
 //Disable Button
-function disableButton(){
-    document.getElementById("submit").disabled = true;
-}
+//function disableButton(){
+//document.getElementById("submit").disabled = true;
+//}
 
 //Activate function
-disableButton();
+//disableButton();
--- js/site_scripts.js
+++ js/site_scripts.js
@@ -1,48 +1,51 @@
-var photos = []; 
-var fileNames = []; 
-var imageList = []; 
-var image; 
-var openList = "<li class='partner'>"; 
-var closeList = "</li>"
+var photos = [];
+var fileNames = [];
+var imageList = [];
+var image;
+var openList = "<li class='partner'>";
+var closeList = "</li>";
 
-for (var i=0; i<6; i++) {
-  fileNames.push("partner"+(i+1));
-  photos.push("<img src='images/partners/"+ fileNames[i]+".png'>");
+for (var i = 0; i < 6; i++) {
+  fileNames.push("partner" + (i + 1));
+  photos.push("<img src='images/partners/" + fileNames[i] + ".png'>");
   image = openList + photos[i] + closeList;
   imageList.push(image);
 }
-  document.getElementById("partners").innerHTML = imageList;
+document.getElementById("partners").innerHTML = imageList;
 
-
-  /*----Random Codes----*/
-  var code = ' ';
-  var getCode = ' ';
-  var btnvalue; //for button boolean value
-  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
-  //determine when to able or disable button
-  function disableButton(btnvalue){
-    document.getElementById("submit").disabled = btnvalue;
-    if(btnvalue == true){
-      //set  button and label color translucent
-      document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
-      document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
-    } else{
-      //set button and label color with no transparency
-      document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
-      document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";   
-    }
+/*----Random Codes----*/
+var code = " ";
+var getCode = " ";
+var btnvalue; //for button boolean value
+var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+//determine when to able or disable button
+function disableButton(btnvalue) {
+  document.getElementById("submit").disabled = btnvalue;
+  if (btnvalue == true) {
+    //set  button and label color translucent
+    document.getElementById("submit").style.backgroundColor =
+      "rgba(73, 119, 209, 0.3)";
+    document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+  } else {
+    //set button and label color with no transparency
+    document.getElementById("submit").style.backgroundColor =
+      "rgba(73, 119, 209,1)";
+    document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
   }
-  //Listen to user input code
-  var codebox = document.getElementById("codeentered"); //get textbox
-  codebox.addEventListener("input", evaluateCode);
+}
+disableButton(btnvalue);
+//Listen to user input code
+var codebox = document.getElementById("codeentered"); //get textbox
+codebox.addEventListener("input", evaluateCode);
 
 //run function if detected user had entered a character in textbox
-function evaluateCode(){
+function evaluateCode() {
   getCode = document.getElementById("codeentered").value; //get character entered
   var charset1 = getCode.trim(); //remove any hidden characters entered
   var charset2 = code.trim(); //remove any hidden characters generated
   //test if code entered matches the number of generated characters
-  if(charset1.length == charset2.length && charset1 == charset2){
+  if (charset1.length == charset2.length && charset1 == charset2) {
     disableButton(false); //if match, run the function to enable button
   }
 }
+evaluateCode();
