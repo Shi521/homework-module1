500473f89d1d5b5223244a9821af535bfeae17f5
Shi521 <h.shih.159@westcliff.edu>
2021-10-05 16:07:08 -0700

    w5d3 homework

--- js/activity.js
+++ js/activity.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
-    $(document).bind("click",function(e){
-        $(e.target).closest(".click").toggleClass("highlight");
-    })
-})
+    $('td').click(function(){
+        var content = $(this).text();
+        var columIndex = $(this).index();
+        var columTitle = $('th:eq(' + columIndex + ')').text();
+         
+        
+        if (content !="Not Available"){
+            $(this).toggleClass('highlight');
+       
+        if($(this).hasClass('highlight')){
+            $('#displaySelected').css('visibility','visible');
+            $('#displaySelected').css('margin-top','2em');
+            $('#result').append('<p>'+content+' at '+ columTitle +'</p>');
+        } else{
+            $('#result p:contains('+content+')').remove();
+            
+            if($('#result').has('p').length == false) {
+                $('#displaySelected').css('visiblity','hidden');
+                $('#displaySelected').css('margin-top','0');
+            }
+        }
+    }
+    });
+});
+
